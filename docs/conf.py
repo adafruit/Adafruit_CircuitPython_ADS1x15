f39188f4ed0c412bdeb29bfe71ecf80c298bce4b
Brennen Bearnes <bbearnes@gmail.com>
2018-07-02 15:15:37 -0600

    add setup.py, add pypi to travis.yml, misc. tweaks for pypi
    Changes per instructions here:
    
    https://github.com/adafruit/circuitpython/issues/979
    
    ...builds locally.  Let's see what Travis thinks.

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,6 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
-# Uncomment the below if you use native CircuitPython modules such as
-# digitalio, micropython and busio. List the modules you use. Without it, the
-# autodoc module docs will fail to generate with a warning.
-autodoc_mock_imports = ["adafruit_bus_device", "micropython"]
-
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),'Register': ('https://circuitpython.readthedocs.io/projects/register/en/latest/', None),'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
 
 # Add any paths that contain templates here, relative to this directory.

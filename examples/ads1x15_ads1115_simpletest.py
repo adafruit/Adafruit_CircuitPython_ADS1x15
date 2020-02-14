d39520c31baf910a7abc803c02dda35c54de737e
fpereme <florian.pereme@altran.com>
2020-02-14 13:37:55 +0100

    modify examples/ads1x15_ads1115_simpletest.py and add a comment for specific address selection

--- examples/ads1x15_ads1115_simpletest.py
+++ examples/ads1x15_ads1115_simpletest.py
@@ -9,6 +9,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
+# you can specify an I2C adress instead of the default 0x48
+#ads = ADS.ADS1115(i2c, address=0x49)
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)

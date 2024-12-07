500d98c94c4ffffd3dd7942cdb048bd94910b15e
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-12-07 12:17:01 +0530

    make pylint & CI happy

--- adafruit_ads1x15/__init__.py
+++ adafruit_ads1x15/__init__.py
@@ -1,3 +1,17 @@
-from .ads1015 import ADS1015 as ADS1015
-from .ads1115 import ADS1115 as ADS1115
-from .analog_in import AnalogIn as AnalogIn
+# SPDX-FileCopyrightText: 2018 Carter Nelson for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_ads1x15`
+====================================================
+
+Support for the ADS1x15 series of analog-to-digital converters.
+
+* Author(s): Carter Nelson
+"""
+from .ads1015 import ADS1015
+from .ads1115 import ADS1115
+from .analog_in import AnalogIn
+
+__all__ = ["ADS1015", "ADS1115", "AnalogIn"]
